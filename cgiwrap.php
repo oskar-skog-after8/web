8a54269423bc45d92d09a2f72ecaa07acf62ace1
oskar skog <oskarskogfinland@gmail.com>
2017-03-12 00:16:14 +0200

    cgiwrap.php: Pretty much finished now

--- cgiwrap.php
+++ cgiwrap.php
@@ -1,11 +1,69 @@
 <?php
 
-/* CONFIGURATION */
+/*
+ * CGI wrapper in PHP
+ *
+ * This script is intended to be used on web servers where CGI support is
+ * unavailable or disabled.
+ *
+ * NOTICE:  This requires the proc_open function.
+ */
+
+/*
+ * Copyright (c) Oskar Skog, 2017
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1.  Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ * 2.  Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ * This software is provided by the copyright holders and contributors "as is"
+ * and any express or implied warranties, including, but not limited to, the
+ * implied warranties of merchantability and fitness for a particular purpose
+ * are disclaimed. In no event shall the copyright holder or contributors be
+ * liable for any direct, indirect, incidental, special, exemplary, or
+ * consequential damages (including, but not limited to, procurement of
+ * substitute goods or services; loss of use, data, or profits; or business
+ * interruption) however caused and on any theory of liability, whether in
+ * contract, strict liability, or tort (including negligence or otherwise)
+ * arising in any way out of the use of this software, even if advised of the
+ * possibility of such damage.
+ */
+
+
+/*
+ *      ***               ***
+ *      *** CONFIGURATION ***
+ *      ***               ***
+ */
+
+$err_contact = "spam@example.com";
+/*
+ * wrap_conf:
+ * REQUEST_URI to script command substitution list
+ *
+ * The first regex pattern that mathces the REQUEST_URI will be used to
+ * regex-substitute it to a shell command.  This shell command should be
+ * the CGI script you wish to wrap.
+ *
+ * If you use URL rewriting, this one PHP script can wrap all your CGI
+ * scripts.
+ */
 $wrap_conf = array(
     array("~^/oskar/cgiwrap\\.php(\\?test.*)?$~", "./test2.py"),
     array("~^.*\\?hello$~", "./ctest"),
 );
-$err_contact = "spam@example.com";
+
+/*
+ *      ***                   ***
+ *      *** CONFIGURATION END ***
+ *      ***                   ***
+ */
 
 
 function error($msg)
@@ -98,20 +156,47 @@ function HTTP_response($raw)
 /* Inner main function. */
 function wrap($script)
 {
+    /*
+     * $_SERVER is pretty much the same as the environment variables sent
+     * to CGI scripts.
+     */
     $env = $_SERVER;
     $env['QUERY_STRING'] = make_query_string($_GET);
-    $pipecfg = array(0 => array("pipe", "r"), 1 => array("pipe", "w"));
+    /* TODO:  Check if QUERY_STRING needs to be overriden. */
+    
+    $pipecfg = array(
+        0 => array("pipe", "r"), /* POST parameters */
+        1 => array("pipe", "w"),
+        2 => array("pipe", "w"), /* error handling */
+    );
     $process = proc_open($script, $pipecfg, $pipes, NULL, $env);
     if (!is_resource($process))
     {
-        error("Can't launch");
+        error("proc_open failed");
         return;
     }
+    
+    /*
+     * CGI scripts read the HTTP body from stdin. POST parameters are
+     * stored in the HTTP body using the same syntax as GET parameter.
+     */
     fwrite($pipes[0], make_query_string($_POST));
     fclose($pipes[0]);
-    HTTP_response(stream_get_contents($pipes[1]));
+    
+    $stdout = stream_get_contents($pipes[1]);
+    $stderr = stream_get_contents($pipes[2]);
     fclose($pipes[1]);
+    fclose($pipes[2]);
     proc_close($process);
+    /*
+     * proc_open doesn't fail even if the script does.
+     * If no data was returned, something probably went wrong and the
+     * reason is on stderr.
+     */
+    if ($stdout)
+        HTTP_response($stdout);
+    else
+        error("CGI execution failed\n\n" . $stderr);
 }
 
 function main()
